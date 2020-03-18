ad3384334ce5c1e0e5d4155e5553356480c2affe
jack7cz <prerost.j@seznam.cz>
2020-03-18 13:57:09 +0100

    Initial files

---
+++ NameEditor.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>
+      First name:
+      <input v-model="editableFirstName" />
+      Last name:
+      <input v-model="editableLastName" />      
+    </p>
+    <p><strong>Output:</strong><br/>
+      {{ editableFirstName + ' ' + editableLastName }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NameEditor',
+  data () {
+    return {
+      editableFirstName: this.firstName,
+      editableLastName: this.lastName
+    }
+  },
+  props: {
+    firstName: {
+      type: String,
+      default: 'John'
+    },
+    lastName: {
+      type: String,
+      default: 'Doe'
+    }    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NameEditor :firstName="firstName" :lastName="lastName" />
+  </div>
+</template>
+
+<script>
+import NameEditor from './NameEditor'
+
+export default {
+  components: { NameEditor },
+  data: function () {
+    return {
+      firstName: 'Riki',
+      lastName: 'Fridrich',     
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
