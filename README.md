87abf908cd15fbdcf07c5952186f33764cd0e8f7
jack7cz <prerost.j@seznam.cz>
2020-03-20 19:47:55 +0100

    Update after Lesson 2

--- README.md
+++
@@ -1,2 +0,0 @@
-# workshop-vue-mall_JAKUB
- 
--- index.vue
+++ index.vue
@@ -1,18 +1,33 @@
 <template>
   <div>
-    <NameEditor :firstName="firstName" :lastName="lastName" />
+    <p>{{ fullName }}</p>
+    <p>
+      Full name:
+      <input type="text" v-model="fullName">
+    </p>
+    <NameEditor v-model="fullName" />
   </div>
 </template>
 
 <script>
+
+/*
+ÚKOL
+
+Vytvorte komponentu AddressEditor pre editáciu adresy, ktorú dostanete vo formáte:
+U garáží 1611/1
+
+Chceme zvlášť políčka pre ulicu, číslo popisné, číslo orientačné.
+*/
+
 import NameEditor from './NameEditor'
 
 export default {
   components: { NameEditor },
   data: function () {
     return {
-      firstName: 'Riki',
-      lastName: 'Fridrich',     
+      fullName: 'Riki Fridrich',
+      address: 'U garáží 1611/1'
     }
   }
 }
