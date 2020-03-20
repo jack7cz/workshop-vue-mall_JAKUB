f120ef78103c06c8cbb4af4514edcedfaf83c1f4
Jakub Nedorost <jakub.nedorost@mallgroup.com>
2020-03-20 19:00:11 +0100

    g

---
+++ index2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Value :value="value1"/>
+  </div>
+</template>
+
+<script>
+import Value from './value'
+
+export default {
+  components: { Value },
+  data: function () {
+    return {
+      value1: 'hodnota z index 2'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ value.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>
+      {{ value }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Value',
+  data () {
+    return {
+    }
+  },
+  props: {
+    value: {
+      type: String,
+      default: 'Hodnota'
+    } 
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
